bdf6e48570c9822eaeff6fafffd250c3e9c07c1c
TempestRules <vielm18@student.sdu.dk>
2018-11-09 17:57:21 +0100

    Project and basic math

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+//@author Victor Elmkvist
+public class Calculator extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
