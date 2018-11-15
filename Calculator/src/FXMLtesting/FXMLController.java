c3ed1ed4033a781d36102558e894518d5f5ab63e
TempestRules <43045051+TempestRules@users.noreply.github.com>
2018-11-15 14:47:17 +0100

    FXML Controller
    Controller Update

--- Calculator/src/FXMLtesting/FXMLController.java
+++ Calculator/src/FXMLtesting/FXMLController.java
@@ -1,20 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FXMLtesting;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
-/**
- * FXML Controller class
- *
- * @author velmk
- */
 public class FXMLController implements Initializable {
+
+    @FXML
+    private Button buttonZero;
+    @FXML
+    private Button buttonOne;
+    @FXML
+    private Button buttonTwo;
+    @FXML
+    private Button buttonThree;
+    @FXML
+    private Button buttonFour;
+    @FXML
+    private Button buttonFive;
+    @FXML
+    private Button buttonSix;
+    @FXML
+    private Button buttonSeven;
+    @FXML
+    private Button buttonEight;
+    @FXML
+    private Button buttonNine;
+    @FXML
+    private Button buttonLeftParentathis;
+    @FXML
+    private Button buttonRightParenthathis;
+    @FXML
+    private Button buttonComma;
+    @FXML
+    private Button buttonEquals;
+    @FXML
+    private Button buttonPlus;
+    @FXML
+    private Button buttonMinus;
+    @FXML
+    private Button buttonMultiplication;
+    @FXML
+    private Button buttonDivide;
+    @FXML
+    private Button buttonPI;
+    @FXML
+    private Pane centerPane;
+    @FXML
+    private TextField txtFieldInput1;
+    @FXML
+    private TextField txtFieldInput2;
+    @FXML
+    private Label labelResult;
+    @FXML
+    private TextField txtFieldResult;
 
     /**
      * Initializes the controller class.
@@ -22,6 +67,86 @@ public class FXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void handleButtonZeroAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonOneAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonTwoAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonThreeAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonFourAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonFiveAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonSixAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonSevenAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonEightAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonNineAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonLeftParenthathisAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonEqualsAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonPlusAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonMinusAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonMultiplicationAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonDivideAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleButtonPIAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleTxtFieldInput1Action(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleTxtField2Action(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleTxtFieldResultAction(ActionEvent event) {
+    }
+
 }
