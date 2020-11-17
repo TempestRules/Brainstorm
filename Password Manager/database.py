281a38698fce7a8ab871b32684b6032d87617a6c
Victor <velmkvist@gmail.com>
2020-11-17 23:13:44 +0100

    Added connections for mysql.
    Connections are not tested yet. Menu has been filled out with the functions.

--- Password Manager/database.py
+++ Password Manager/database.py
@@ -1,1 +1,54 @@
-//Shall contain connection functions to the database
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    password="yourpassword"
+)
+
+# Storing given information in database.
+def storeAccount(username, passwd, email, appName, url):
+    mycursor = mydb.cursor()
+
+    accountQuery = "INSERT INTO accounts (username, password, email, appName, url) VALUES (%s, %s, %s, %s, %s)"
+    values = (username, passwd, email, appName, url)
+
+    mycursor.execute(accountQuery, values)
+    mydb.commit()
+
+# Retrieves password based on application name.
+def getPasswd(appName):
+    try:
+        mycursor = mydb.cursor()
+        passwdQuery = "SELECT password FROM accounts WHERE appName LIKE '%s'"
+    
+        mycursor.execute(passwdQuery, appName)
+        myresult = mycursor.fetchall()
+
+        print("Your password is: ")
+        print(myresult[0])
+    
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+
+# Retrives all accounts created with given email.
+def getAccounts(email):
+    info = ('Username: ', 'Password: ', 'Email: ', 'App/Site: ', 'Url: ')
+
+    try:
+        mycursor = mydb.cursor()
+        accountQuery = "SELECT * FROM accounts WHERE email LIKE '%s'"
+    
+        mycursor.execute(accountQuery, email)
+        myresult = mycursor.fetchall()
+
+        print('')
+        print('Accounts: ')
+        print('')
+        for row in myresult:
+            for i in range(0, len(row)-1):
+                print(info[i] + row[i])
+        print('')
+        print('-'*30)
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
--- Password Manager/menu.py
+++ Password Manager/menu.py
@@ -1,42 +1,45 @@
-
+from database import storeAccount, getAccounts, getPasswd
 
 # Shows a basic menu with options for the user.
 def menu():
-    print('-'*30);
-    print(('-'*13) + 'Menu' + ('-'*13));
-    print('1. Add a new login');
-    print('2. Show all logins with a specific email');
-    print('3. Show password for a specific site or application');
-    print('Q. Exit');
-    print('-'*30);
-    return input(': ');
+    print('-'*30)
+    print(('-'*13) + 'Menu' + ('-'*13))
+    print('1. Add a new login')
+    print('2. Show all logins with a specific email')
+    print('3. Show password for a specific site or application')
+    print('Q. Exit')
+    print('-'*30)
+    return input(': ')
 
 # Creates a new login and stores it in the database.
-def addLogin():
-    print('Please provide the name of the site or application');
-    appName = input();
-    print('Please provide the password for the account: ');
-    password = input();
+def addAccount():
+    print('Please provide the name of the site or application')
+    appName = input()
+    print('Please provide the password for the account: ')
+    password = input()
     # hashedPasswd = HASH THE GIVEN PASSWORD
 
-    print('-'*30);
-    print('Please provide the email used to create the account');
-    email = input();
-    print('Please provide the username for the account');
-    username = input();
+    print('-'*30)
+    print('Please provide the email used to create the account')
+    email = input()
+    print('Please provide the username for the account')
+    username = input()
     print('Please provide the url to the site the account is created with')
-    url = input();
-    # STORE PASSWORD FUNCTION
+    url = input()
+    # Database function, stores given info.
+    storeAccount(username, password, email, appName, url)
 
 # Returns the password based on the site the account belongs to.
-def getPasswd():
-    print('Please provide the name of the site or application the account belongs to');
-    appName = input();
-    # FIND PASSWORD FUNCTION
+def getPassword():
+    print('Please provide the name of the site or application the account belongs to')
+    appName = input()
+    # Database function, returns password based on application.
+    getPasswd(appName)
 
 # Returns all accounts connected to a specific email.
-def getAccounts():
+def getLogins():
     print('Please provide the email you wish to see accounts for')
-    email = input();
-    # FIND ACCOUNTS FUNCTION
+    email = input()
+    # Database function, returns accounts based on email.
+    getAccounts(email)
     
