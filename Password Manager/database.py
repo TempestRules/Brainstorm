64ad967837335df795fd82c7f83d5a9b19298d11
Victor <velmkvist@gmail.com>
2020-11-18 00:52:51 +0100

    Dockerfile added
    Docker is to be used for the mysql database.

--- Password Manager/database.py
+++ Password Manager/database.py
@@ -2,8 +2,9 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
     host="localhost",
-    user="yourusername",
-    password="yourpassword"
+    user="root",
+    password="secure",
+    datebase="accounts"
 )
 
 # Storing given information in database.
