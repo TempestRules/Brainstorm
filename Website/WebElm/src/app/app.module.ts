201d170349a97056464d97594e9a97e2586f3c20
Victor <vielm18@student.sdu.dk>
2020-11-27 20:59:03 +0100

    Home component
    Added routing

--- Website/WebElm/src/app/app.module.ts
+++ Website/WebElm/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HangmanComponent } from './hangman/hangman.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HangmanComponent
+    HangmanComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
